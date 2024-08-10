4d8e663e3347f5175a2b705640a700b1965aa664
heyRickDev <henriquevanildodasilva@gmail.com>
2024-08-10 00:00:03 -0300

    create post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Header } from './components/Header'
 import { Post } from './components/Post'
-import { saintsPhrase } from './components/Phrase'
 import { Sidebar } from './components/Sidebar'
 import styles from './App.module.css'
 import './global.css'
@@ -14,21 +13,11 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-      author={saintsPhrase[0].author} 
-      content={saintsPhrase[0].phrase} 
-      />
-      <Post 
-      author={saintsPhrase[8].author} 
-      content={saintsPhrase[8].phrase} 
-      />
+          <Post />
+          <Post />
+          
         </main>
-      </div>
-
-      
-      
-      
-      
+      </div>      
     </>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,35 @@
-export function Post(props) {
+import styles from './Post.module.css'
+
+export function Post() {
     return (
-        <>
-            <strong>{props.author}</strong>
-            <p>{props.content}</p>
-        </>
+            <article>
+                <div className={styles.header}>
+                    <div className={styles.author}>
+                        <img src="https://avatars.githubusercontent.com/u/151173750?v=4" alt="" />
+                        <div className={styles.authorInfo}>
+                            <strong>Henrique Silva</strong>
+                            <span>Jr. Web Developer</span>
+                        </div>
+                    </div>
+                <div>
+                    <time title="09 de agosto às 20h" dateTime="">Publicado há 1h</time>
+                </div>
+                </div>
+                <div className={styles.textPost}>
+                    <p>Fala galeraa 👋</p>
+                    <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                    <p>
+                        <a href="">👉 jane.design/doctorcare</a>
+                    </p>
+                    <p>
+                        <a href="">#novoprojeto #nlw #rocketseat</a>
+                    </p>
+                </div>
+                <div className={styles.comments}>
+                    <strong>Deixe seu feedback</strong>
+                    <textarea placeholder='Digite seu comentário'></textarea>
+                    <button>Publicar</button>
+                </div>
+            </article>
     )
 }
